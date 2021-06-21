ce20bc0a1fcb17b69ee66c9161afea3fa8076863
Isakov Nikita <isakov4352@gmail.com>
2021-06-22 00:03:37 +0700

    Сделал плавный оверлей для страницы история

--- source/js/modules/common.js
+++ source/js/modules/common.js
@@ -1,11 +1,10 @@
 export default () => {
     window.onload = function() {
         document.querySelector('html').classList.add('js-load');
-        if (document.location.hash != undefined) {
+        if (document.location.hash != undefined && document.querySelector('.js-menu-link[href*="' + document.location.hash + '"]') != null) {
             document.querySelector('.js-menu-link[href*="' + document.location.hash + '"]').classList.add('active');
         } else {
             document.querySelector('.page-header__menu').querySelector('.js-menu-link').classList.add('active');
         }
-        
     }
 };
